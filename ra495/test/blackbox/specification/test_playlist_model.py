1479184fe539b7bae3937698377dfece3ddf384b
Raiyan805 <raiyan.alam805@gmail.com>
2025-12-29 11:26:00 +0000

    Add initialisation tests for Playlist class in test_playlist_model.py

--- ra495/test/blackbox/specification/test_playlist_model.py
+++ ra495/test/blackbox/specification/test_playlist_model.py
@@ -15,3 +15,22 @@ class TestPlaylistModel(unittest.TestCase):
     def setUp(self):
         self.t1 = Track(Path("1.mp3"), "T1", "A1", 60)
         self.t2 = Track(Path("2.mp3"), "T2", "A1", 90)
+
+    # Initialisation Tests
+
+    def test_init_valid(self):
+        """
+        Expected Result: Object created with correct name and empty track list.
+        Actual Result: Name='My Mix', Tracks=[]
+        """
+        pl = Playlist("My Mix")
+        self.assertEqual(pl.name, "My Mix")
+        self.assertEqual(pl.tracks, [])
+
+    def test_init_empty_name(self):
+        """
+        Expected Result: Name defaults to "(unnamed)".
+        Actual Result: Name='(unnamed)'
+        """
+        pl = Playlist("")
+        self.assertEqual(pl.name, "(unnamed)")
