07eb41fe99cc56db2b64ef018c15ead5b024ecaa
Raiyan805 <raiyan.alam805@gmail.com>
2025-12-29 16:03:27 +0000

    added initialised testing for test_playlist_model_branch.py

--- ra495/test/whitebox/branch/test_playlist_model_branch.py
+++ ra495/test/whitebox/branch/test_playlist_model_branch.py
@@ -14,3 +14,27 @@ class TestPlaylistModelBranch(unittest.TestCase):
     def setUp(self):
         self.t1 = Track(Path("1.mp3"), "T1", "A1", 60)
 
+    def test_init_branches(self):
+        """
+        Branches:
+         - if name not str
+         - name.strip() or fallback
+         - if tracks is None / not list
+        """
+        # Name Type (True)
+        p1 = Playlist(999)
+        self.assertEqual(p1.name, "999")
+        # Name Fallback (True)
+        p2 = Playlist("   ")
+        self.assertEqual(p2.name, "(unnamed)")
+
+        # Tracks Validation
+        # True (None)
+        p3 = Playlist("A", None)
+        self.assertEqual(p3.tracks, [])
+        # Elif (Not List)
+        p4 = Playlist("B", "NotList")
+        self.assertEqual(p4.tracks, [])
+        # Else (Valid)
+        p5 = Playlist("C", [self.t1])
+        self.assertEqual(len(p5.tracks), 1)
