03b7ccd75b903f97cffa91ba8eaf0a568f4e7401
Raiyan805 <raiyan.alam805@gmail.com>
2025-12-30 00:00:00 +0000

    Created test_read_metadata_branches in test_library_branch.py

--- ra495/test/whitebox/branch/test_library_branch.py
+++ ra495/test/whitebox/branch/test_library_branch.py
@@ -11,3 +11,17 @@ class TestLibraryBranch(unittest.TestCase):
     Test Technique: Branch Testing (White-Box)
     """
 
+    def test_read_metadata_branches(self):
+        """
+        Branches:
+         - if not HAS_MUTAGEN
+         - if audio is None
+         - if info is not None
+         - if hasattr(info, "length")
+         - if tags
+         - if "TIT2" in tags
+         - if "TPE1" in tags
+        Expected Result: Returns defaults for missing data and correct values for valid tags.
+
+        """
+        path = Path("test.mp3")
