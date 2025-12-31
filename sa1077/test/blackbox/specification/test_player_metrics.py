f1de653b28fd23747bda8c13b3692ef11d822f68
Samuel <sameyaw150@gmail.com>
2025-12-31 17:35:40 +0000

    Implemented Category Partition test class and setup

--- sa1077/test/blackbox/specification/test_player_metrics.py
+++ sa1077/test/blackbox/specification/test_player_metrics.py
@@ -19,3 +19,18 @@ class MockState:
         self.play_counts = {}
         self.library_tracks = []
         self.current_track = None
+# Test Class
+
+class TestPlayerMetricsSpecs(unittest.TestCase):
+    """
+        Black-box specification-based test for player_metrics.py.
+        Technique: Category Partition Method
+        Tools: Python unittest + unittest.mock
+        Source: playerMetrics.txt TSL Generatored Test Frames
+    """
+
+    def setUp(self):
+        self.mock_state = MockState()
+        self.captured_output = io.StringIO()
+        self.sys_stdout = sys.stdout
+        sys.stdout = self.captured_output
