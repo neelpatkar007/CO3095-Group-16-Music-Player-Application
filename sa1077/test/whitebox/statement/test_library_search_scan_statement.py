9dee5eff87369a6ad9c1e44940bcb63c95655141
Samuel <sameyaw150@gmail.com>
2026-01-01 18:38:08 +0000

    Verify statements for data aggregation and reporting

--- sa1077/test/whitebox/statement/test_library_search_scan_statement.py
+++ sa1077/test/whitebox/statement/test_library_search_scan_statement.py
@@ -48,3 +48,46 @@ class TestLibrarySearchScanStatement(unittest.TestCase):
         t1 = MockTrack("/music/song1.mp3", "Love Song", "The Band", 200)
         self.mock_state.library_tracks = [t1]
         library_search_scan.search_library(self.mock_state, "love")
+
+    def test_view_songs_table(self):
+        """
+        Expected Result: Prints all songs.
+        Actual Result:
+        PASSED [100%][lib] Songs (library):
+         No  Title                           Artist                  Time
+        -----------------------------------------------------------------
+          1  Title                           Artist                 01:00
+        """
+        self.mock_state.library_tracks = [MockTrack("/a.mp3")]
+        library_search_scan.view_songs_table(self.mock_state)
+
+    def test_view_artists_table(self):
+        """
+        Expected Result: Groups by artist and prints artist table.
+        Actual Result:
+        Artist                     Tracks      Time
+        ---------------------------------------------
+        Artist X                        2     02:30
+        Artist Y                        1     00:10
+
+        """
+        t1 = MockTrack("/a.mp3", artist="Artist X", duration=100)
+        t2 = MockTrack("/b.mp3", artist="Artist X", duration=50)
+        t3 = MockTrack("/c.mp3", artist="Artist Y", duration=10)
+        self.mock_state.library_tracks = [t1, t2, t3]
+
+        library_search_scan.view_artists_table(self.mock_state)
+
+    def test_view_albums_table(self):
+        """
+        Expected Result: Groups by album and prints table.
+        Actual Result:
+        Album (folder)             Tracks      Time
+        ---------------------------------------------
+        Album A                         2     02:00
+        """
+        t1 = MockTrack("/Music/Album A/01.mp3", duration=60)
+        t2 = MockTrack("/Music/Album A/02.mp3", duration=60)
+        self.mock_state.library_tracks = [t1, t2]
+
+        library_search_scan.view_albums_table(self.mock_state)
