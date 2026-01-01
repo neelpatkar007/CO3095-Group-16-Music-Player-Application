050b2f6ee19d76e6089901ef7e3de3b1e17fe0bf
Neel <np343@student.le.ac.uk>
2026-01-01 21:08:58 +0000

    Add white-box statement tests for player_seek.py in test_player_seek_statement.py

--- np343/test/whitebox/statement/test_player_seek_statement.py
+++ np343/test/whitebox/statement/test_player_seek_statement.py
@@ -3,3 +3,10 @@ from unittest.mock import MagicMock, patch
 from music_player import player_seek
 from music_player.player_state import PlayerState
 from music_player.library import Track
+
+class TestPlayerSeekStatement(unittest.TestCase):
+    """
+    White-Box Statement Tests for player_seek.py.
+    Testing Tool: Python unittest + unittest.mock
+    Test Technique: White-Box Statement Testing
+    """
