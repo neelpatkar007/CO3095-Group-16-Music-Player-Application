e30f30ca7eb37945a1c92184247a054ec1bb43d0
Neel <np343@student.le.ac.uk>
2026-01-01 20:21:19 +0000

    Add whitebox branch tests for player queue functionality

--- np343/test/whitebox/branch/test_player_queue_branch.py
+++ np343/test/whitebox/branch/test_player_queue_branch.py
@@ -22,3 +22,32 @@ class MockAudioEngine:
 
     def is_busy(self):
         return False
+
+class TestPlayerQueueCoverage(unittest.TestCase):
+    """
+    Whitebox Branch Testing for player_queue.py.
+    Tool: Python unittest + unittest.mock
+    Technique: White-Box Branch Testing
+    """
+
+    def setUp(self):
+        """
+        Setup valid PlayerState with 3 tracks for each test.
+        """
+        self.track1 = Track(Path("song1.mp3"), "Song 1", "Artist 1", 180)
+        self.track2 = Track(Path("song2.mp3"), "Song 2", "Artist 2", 200)
+        self.track3 = Track(Path("song3.mp3"), "Song 3", "Artist 3", 210)
+
+        self.state = MagicMock()
+        self.state.tracks = [self.track1, self.track2, self.track3]
+        self.state.library_tracks = [self.track1, self.track2, self.track3]
+        self.state.current_index = 0
+        self.state.current_track = self.track1
+        self.state.history = []
+        self.state.audio_engine = MockAudioEngine()
+        self.state.is_playing = False
+        self.state.is_paused = False
+        self.state.loop_mode = "off"
+        self.state.shuffle_active = False
+        self.state.position_seconds = 0.0
+        self.state.playlists = []
