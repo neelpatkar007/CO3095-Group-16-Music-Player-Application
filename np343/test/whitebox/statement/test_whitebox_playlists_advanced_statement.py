01ccc7a6e3a8b4226aa0718f15e50aed6e7b89ad
Neel <np343@student.le.ac.uk>
2026-01-01 21:24:30 +0000

    Add setup method to white-box tests for playlists_advanced.py to initialise player state and playlists

--- np343/test/whitebox/statement/test_whitebox_playlists_advanced_statement.py
+++ np343/test/whitebox/statement/test_whitebox_playlists_advanced_statement.py
@@ -8,3 +8,10 @@ class TestPlaylistsAdvancedStatement(unittest.TestCase):
     White-Box Statement Coverage for playlists_advanced.py.
     Targets: Missing error blocks in _get_playlist and copy/merge validations.
     """
+
+    def setUp(self):
+        self.state = PlayerState([], MagicMock())
+        # Populate playlists to pass initial availability checks
+        self.pl1 = MagicMock()
+        self.pl1.name = "One"
+        self.state.playlists = [self.pl1]
